a07e4c2b2979f1fddb5fd168ca6c3c96d3a0332b
Dyegho <dyeghocunha@gmail.com>
2023-04-26 19:53:51 -0300

    Banner Finalizado

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,23 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
 
-export default function PaginaInicial(){
-  return(
-    <Cabecalho/>
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A Galeria mais completa do espaco</h1>
+            <img src={banner} alt=" Imagem da terra vista do espaco" />
+          </div>
+
+        </section>
+
+      </main>
+    </>
   )
 }
