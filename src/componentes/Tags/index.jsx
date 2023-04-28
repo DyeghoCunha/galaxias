ddd8d0a637a65dfedcfd0fed0bab3ea15b69756a
Dyegho <dyeghocunha@gmail.com>
2023-04-28 08:58:33 -0300

    alteracao no Default do Filtro

--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -3,6 +3,14 @@ import style from './Tags.module.scss'
 import fotos from '../Galeria/fotos.json'
 
 export default function Tags({tags, filtraFotos, setItens}) {
+
+function filtro(event){
+  event.preventDefault();
+  event = setItens(fotos)
+  return event;
+
+}
+
   return (
     <div className={style.tags}>
       <p>Filtre por composição:</p>
@@ -10,7 +18,8 @@ export default function Tags({tags, filtraFotos, setItens}) {
         {tags.map((tag =>{
           return <li key ={tag} onClick={()=>filtraFotos(tag)}>{tag}</li>
         }))}
-        <li onClick={()=>setItens(fotos)}>Todos</li>
+        {/* <li onClick={()=>setItens(fotos)}>Todos</li> */}
+        <li onClick={filtro}>Todos</li>
       </ul>
     </div>
   )
